99f9a50ef8f1a1923522453ae9c5b721696bb07d
Lawen Liu <lwc541117@gmail.com>
2019-06-26 14:57:29 +0800

    Update wkhtmltopdf.go
    update command starting up with xvfb for debian without display

--- wkhtmltopdf.go
+++ wkhtmltopdf.go
@@ -61,7 +61,10 @@ var (
 
 	// Executable is the command to run wkhtmltopdf. If wkhtmltopdf
 	// cannot be found on your path, amend this to its location.
-	Executable = "wkhtmltopdf"
+	// Executable = "wkhtmltopdf"
+	
+	// Enabled for debian without display, we have to use xvfb to start wkhtmltopdf
+	Executable = "xvfb wkhtmltopdf"
 
 	// TempDir is where the directories for creating temporary
 	// files are created.
